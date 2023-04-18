0806ec98814e56c0f9b7fb551da1f8495c14e1af
Laura Hope <127255399+lauramhope@users.noreply.github.com>
2023-04-18 15:59:27 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import WeatherService from './weather-service.js';
+
+// Business Logic
+
+function getWeather(city) {
+  let promise = WeatherService.getWeather(city);
+  promise.then(function(weatherDataArray) {
+    printElements(weatherDataArray);
+  }, function(errorArray) {
+    printError(errorArray);
+  });
+}
+
+// UI Logic
+
+function printElements(data) {
+  document.querySelector('#showResponse').innerText = `The humidity in ${data[1]} is ${data[0].main.humidity}%.
+  The temperature in Kelvins is ${data[0].main.temp} degrees.`;
+}
+
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${error[2]}: ${error[0].status} ${error[0].statusText}: ${error[1].message}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
